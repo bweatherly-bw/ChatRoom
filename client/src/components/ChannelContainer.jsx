6509e3ed0da7a431bbea72a26b98be6ae7f714a1
bweatherly-bw <brandon.weatherly58@gmail.com>
2021-11-02 18:51:43 -0400

    initial commit

---
+++ client/src/components/ChannelContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ChannelContainer = () => {
+    return (
+        <div>
+            React Chatting App, built with React and Stream.io
+        </div>
+    );
+}
+
+export default ChannelContainer;
